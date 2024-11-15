7f32e0de82b71e97dad10c7df9b335701a23edc1
Narayan71432 <152845361+Narayan71432@users.noreply.github.com>
2024-11-15 21:15:18 +0530

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-def get_db_client():
-    client = MongoClient("mongodb://localhost:27017/")
-    return client['survey_database']
-
-def save_survey_data(db, data):
-    collection = db['survey_results']
-    collection.insert_one(data)
